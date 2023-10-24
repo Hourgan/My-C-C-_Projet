2e0360de2f272f4a6189cf86d426426f0ea7d196
Naskar <getemdina@gmail.com>
2023-10-25 00:02:14 +0300

    new

---
+++ HW_2_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+	{
+int x = 0;
+ x = (3*9*(3+(9*3/3)));
+printf("x = %d\n", x);
+return 0;
+}
---
+++ HW_2_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+	{
+int n , m;
+printf("n = ?\n");
+printf("m = ?\n");
+scanf("&d", n);
+scanf("&d", m);
+if(n > m )
+	{
+printf("%d", m);
+}
+else
+	{
+printf("%d" ,n);
+}
+return 0;
+}
